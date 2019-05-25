15a297552f65f5d5ea719b1159485f2de8e92481
boba <poshtarboba@toavalon.com>
2019-05-25 16:07:53 +0300

    imgsrc.ru - new tapeImages (with links, thumbs)

--- imgsrc.ru.js
+++ imgsrc.ru.js
@@ -47,38 +47,102 @@
 	}
 
 	function loadAllImages() {
-		let search = location.search.substr(1).split('&');
-		if (!search.includes('showall=true')) return;
-		let href = [];
-		document.querySelectorAll('a.navi[href]').forEach(function(a) {
-			let url = a.href;
-			if (!href.includes(url)) href.push(url);
-		});
-		let infoText = document.createElement('h1');
-		infoText.innerHTML = 'Preparing: <span id="infoText">1</span> / ' + (href.length + 1);
-		document.body.insertBefore(infoText, document.body.firstElementChild);
-		let style = document.createElement('style');
-		let styleHtml = '\nhtml, body { scroll-behavior: smooth; }\n';
-		styleHtml += 'body { color: #fff; background-color: #000; text-align: center; }\n';
-		styleHtml += 'img[alt="###"] { display: inline-block; margin-bottom: 12px; }\n';
-		styleHtml += '#itape { text-align:center; }\n';
-		styleHtml += '#itape img { display: inline-block; margin-bottom: 20px; max-width: 100%; max-height: 98vh; }\n';
-		styleHtml += '#itape #fullHgh:checked ~ img { max-height: none; }\n';
-		style.innerHTML = styleHtml;
-		document.head.appendChild(style);
-		window.userHref = '/main/user.php?user=';
-		document.querySelectorAll('a').forEach(function (a){
-			let href = a.getAttribute('href');
-			if (href && (href.substr(0, 20) === window.userHref)) window.albumOwner = href.substr(20);
-		});
-		window.topLinks = '<a href="/main/search.php">Search</a> &nbsp;&nbsp;\n';
-		if (window.albumOwner) window.topLinks += '<a href="' + window.userHref + window.albumOwner + '">User ' + window.albumOwner + '</a> &nbsp;&nbsp; \n';
-		let html = '<div id="itape">\n' + window.topLinks + '<span>Keys A, Q for navigation, W - for change view</span>\n';
-		html += '<input type="checkbox" id="fullHgh">&nbsp;Full&nbsp;height&nbsp;&nbsp;&nbsp;&nbsp;\n';
-		html += '<span id="pInfo">Loading images: ';
-		html += '<span id="curInfo"></span> / <span id="allInfo"></span></span><br>\n';
-		document.querySelectorAll('img.big').forEach(function(img) { html += imgHTMLCode(img); });
-		getImagesPage(href, html);
+		const THUMB_HEIGHT = '180px';  // высота миниатюры изображения
+		const THUMB_HEIGHT_LT1 = '140px';
+		const THUMB_HEIGHT_LT2 = '110px';
+		const THUMB_HEIGHT_LT3 = '80px';
+		const THUMB_HEIGHT_GT1 = '220px';
+		const THUMB_HEIGHT_GT2 = '260px';
+		const THUMB_HEIGHT_GT3 = '300px';
+		addCSS();
+		createTape();
+
+		function addCSS(){
+			let css = '\nhtml.smooth-scroll { scroll-behavior: smooth; }\n';
+			css += 'body { margin: 0; padding: 8px; -moz-user-select: none; user-select: none; }\n';
+			css += 'body.tape-images-mode { background: #000; color: #666; }\n';
+			css += 'img { max-width: 100%; }\n';
+			css += '#tapeTools { position: fixed; z-index: 66000; right: 20px; top: 10px; background: #fff; box-shadow: 0 0 5px rgba(0, 0, 0, 0.5); opacity: 0.1; }\n';
+			css += '#tapeTools:hover, #tapeTools.active { opacity: 1; }\n';
+			css += '#tapeTools span { display: block; }\n';
+			css += '#tapeTools .tt-handler { height: 8px; background: #999; cursor: move; }\n';
+			css += '#tapeTools span.tt-mode { padding: 0 4px 2px; font-size: 120%; cursor: pointer; }\n';
+			css += '#tapeTools span.tt-mode:hover { background: #eee; }\n';
+			css += '#tapeTools span.tt-mode.active { background: #ddd; }\n';
+			css += '#tapeTools span.tt-mode.active:hover { background: #ccc; }\n';
+			css += '#tapeTools span.tt-thumb-size { display: none; }\n';
+			css += '#tapeTools span#ttm3.active ~ span.tt-thumb-size { display: block; }\n';
+			css += '#tapeTools span.tt-thumb-size:after { content: ""; display: block; clear: both; }\n';
+			css += '#tapeTools span.tt-thumb-size > span { float: left; width: 35%; text-align: center; border: 1px solid silver; box-sizing: border-box; cursor: pointer; opacity: 0.5; }\n';
+			css += '#tapeTools span.tt-thumb-size > span:hover { background: #eee; opacity: 1; }\n';
+			css += '#tapeTools span.tt-thumb-size > span:active { color: #000; border-color: #000; }\n';
+			css += '#tapeTools span.tt-thumb-size > em { float: left; padding-top: 3px; width: 30%; font-size: 0.7em; font-style: normal; text-align: center; }\n';
+			css += '#tapeImages p { text-align: center; }\n';
+			css += '#tapeImages label { cursor: pointer; }\n';
+			css += '#tapeImages input { position: relative; top: 3px; margin-left: 16px; }\n';
+			css += '#tapeImages img { display: block; margin: 0 auto 20px; max-height: 98vh; }\n';
+			css += '#tapeImages.ti-full-mode img { max-height: none; }\n';
+			css += '#tapeImages.ti-thumb-mode img { display: inline-block; margin: 0 0 4px; max-height: ' + THUMB_HEIGHT + '; cursor: pointer; }\n';
+			css += '#tapeImages.ti-thumb-lt1 img { max-height: ' + THUMB_HEIGHT_LT1 + '; }\n';
+			css += '#tapeImages.ti-thumb-lt2 img { max-height: ' + THUMB_HEIGHT_LT2 + '; }\n';
+			css += '#tapeImages.ti-thumb-lt3 img { max-height: ' + THUMB_HEIGHT_LT3 + '; }\n';
+			css += '#tapeImages.ti-thumb-gt1 img { max-height: ' + THUMB_HEIGHT_GT1 + '; }\n';
+			css += '#tapeImages.ti-thumb-gt2 img { max-height: ' + THUMB_HEIGHT_GT2 + '; }\n';
+			css += '#tapeImages.ti-thumb-gt3 img { max-height: ' + THUMB_HEIGHT_GT3 + '; }\n';
+			css += '#tapeImages.ti-thumb-mode img:not(.full-size):hover { opacity: 0.8; }\n';
+			css += '#tapeImages.ti-thumb-mode img.full-size { position: fixed; z-index: 33; left: 50%; top: 50%; transform: translate(-50%, -50%); max-height: 98vh; box-shadow: 0 0 0 2000px rgba(0, 0, 0, 0.9); }\n';
+			css += '#tapeImages .img-wrap { display: inline-block; position: relative; }\n';
+			css += '#tapeImages .img-btn { position: absolute; right: 0; top: 0; width: 100px; min-height: 100px; opacity: 0; text-align: center; }\n';
+			css += '#tapeImages .img-btn:hover { opacity: 1; }\n';
+			css += '#tapeImages .img-btn a { display: block; margin-bottom: 4px; padding: 8px 0; background: #fff; border: 1px solid silver; opacity: 0.4; text-decoration: none; }\n';
+			css += '#tapeImages .img-btn a:hover { opacity: 1; }\n';
+			css += '#tapeImages .img-btn a:active { color: red; border-color: red; }\n';
+			css += '#tapeImages.ti-thumb-mode .img-btn { display: none; }\n';
+			css += '#tapeImages.ti-thumb-mode img.full-size ~ .img-btn { display: block; position: fixed; z-index: 35; opacity: 0.1; }\n';
+			css += '#tapeImages.ti-thumb-mode img.full-size ~ .img-btn:hover { opacity: 1; }\n';
+			let style = document.createElement('style');
+			style.innerHTML = css;
+			document.head.appendChild(style);
+		}
+
+		function createTape(){
+			let search = location.search.substr(1).split('&');
+			if (!search.includes('showall=true')) return;
+			let href = [];
+			document.querySelectorAll('a.navi[href]').forEach(function(a) {
+				let url = a.href;
+				if (!href.includes(url)) href.push(url);
+			});
+			let infoText = document.createElement('h1');
+			infoText.innerHTML = 'Preparing: <span id="infoText">1</span> / ' + (href.length + 1);
+			document.body.insertBefore(infoText, document.body.firstElementChild);
+			let userHref = '/main/user.php?user=', userName;
+			document.querySelectorAll('a').forEach(function (a){
+				let href = a.getAttribute('href');
+				if (href && (href.substr(0, 20) === userHref)) userName = href.substr(20);
+			});
+			window.topLinks = '<a href="/main/search.php">Search</a> &nbsp;&nbsp;\n';
+			if (userName) window.topLinks += '<a href="' + userHref + userName + '">User ' + userName + '</a> &nbsp;&nbsp; \n';
+			
+			let html = '<div id="tapeImages">\n<p>\n' + window.topLinks';
+			html += '<p>A, Q - prev, next image, W - change mode:\n';
+			html += '<label><input type="radio" id="trm1" name="trm" checked> screen size</label>\n';
+			html += '<label><input type="radio" id="trm2" name="trm"> full size</label>\n';
+			html += '<label><input type="radio" id="trm3" name="trm"> thumb size (+ / -)</label>\n';
+			html += '<span id="pInfo">Loading images: <em id="curInfo"></em> / <em id="allInfo"></em></span>\n';
+			html += '</p>\n';
+			document.querySelectorAll('img.big').forEach(function(img) { html += imgHTMLCode(img); });
+			getImagesPage(href, html);
+		}
+	}
+
+	function imgHTMLCode(img){
+		let src = img.classList.contains('lazyload') ? img.dataset.src : img.getAttribute('src');
+		let url = img.parentElement.nextElementSibling.getAttribute('href');
+		let html = '<span class="img-wrap"><img src="" data-src="' + src + '" alt="###">';
+		html += '<span class="img-btn"><a href="' + src + '" download>Download</a>';
+		html += '<a href="' + url + '">Link</a></span></span>\n';
+		return html;
 	}
 
 	function getImagesPage(href, html) {
@@ -86,7 +150,10 @@
 			clearHeader();
 			document.body.innerHTML = html + '</div>\n<p>' + window.topLinks + '</p>';
 			document.getElementById('allInfo').innerText = document.querySelectorAll('img').length;
-			addImagesNavKeys(); // добавляет навигацию клавишами q, a - вверх/вниз, w - переключить #fullHgh
+			document.body.classList.add('tape-images-mode');
+			document.documentElement.classList.add('smooth-scroll');
+			addTapeTools();
+			addNavKeys();
 			loadNextImage();
 			return;
 		}
@@ -106,15 +173,10 @@
 		setTimeout(function (){ xhr.send(); }, rndTime());
 	}
 
-	function imgHTMLCode(img){
-		let url = img.classList.contains('lazyload') ? img.dataset.src : img.getAttribute('src');
-		return '<img src="" data-src="' + url + '" alt="###"><br>\n';
-	}
-
 	function loadNextImage(){
 		let img = document.querySelector('img[data-src]');
 		if (!img) {
-			document.getElementById('pInfo').innerText = 'All images was loaded';
+			document.getElementById('pInfo').innerText = 'All images was loaded (' + document.querySelectorAll('img').length + ')';
 			return;
 		};
 		img.addEventListener('load', function (){
@@ -196,31 +258,170 @@
 		td.innerHTML += html;
 	}
 
-	function addImagesNavKeys(){
+	function addNavKeys(){
+		let tapeImages = document.getElementById('tapeImages');
+		let radioMode1 = document.getElementById('trm1');
+		let radioMode2 = document.getElementById('trm2');
+		let radioMode3 = document.getElementById('trm3');
+		let itemMode1 = document.getElementById('ttm1');
+		let itemMode2 = document.getElementById('ttm2');
+		let itemMode3 = document.getElementById('ttm3');
+		let thumbInc = document.getElementById('thumbinc');
+		let thumbDec = document.getElementById('thumbdec');
+		let thumbSz = document.getElementById('thumbsz');
+		let images = tapeImages.querySelectorAll('img');
+		let activeImage = 0;
+		radioMode1.addEventListener('click', setMode1);
+		radioMode2.addEventListener('click', setMode2);
+		radioMode3.addEventListener('click', setMode3);
+		itemMode1.addEventListener('click', setMode1);
+		itemMode2.addEventListener('click', setMode2);
+		itemMode3.addEventListener('click', setMode3);
+		thumbInc.addEventListener('click', increaseThumb);
+		thumbDec.addEventListener('click', decreaseThumb);
+		window.addEventListener('scroll', windowScroll);
 		window.addEventListener('keydown', function (e){
 			if (e.keyCode === 81) pressedNavKey('q'); // нажали q
 			if (e.keyCode === 65) pressedNavKey('a'); // нажали a
-			if (e.keyCode === 87) pressedWkey();
+			if (e.keyCode === 87) { // нажали w
+				if (radioMode1.checked) return setMode2();
+				if (radioMode2.checked) return setMode3();
+				if (radioMode3.checked) return setMode1();
+			}
+			if (e.keyCode === 61 || e.keyCode === 107) increaseThumb(); // нажали +
+			if (e.keyCode === 173 || e.keyCode === 109) decreaseThumb(); // нажали -
 		});
-		function pressedWkey(){
-			let input = document.getElementById('fullHgh');
-			if (input) input.checked = !input.checked;
-		}
-		function pressedNavKey(key){
-			if (document.images.length < 1) return;
+		tapeImages.querySelectorAll('img').forEach(function (img){
+			img.addEventListener('click', imgClick);
+		});
+		function windowScroll(){
+			if (images.length < 1) return;
+			if (tapeImages.classList.contains('ti-thumb-mode')) return;
 			let halfScreen = window.innerHeight / 2;
 			let centerPos = halfScreen + window.scrollY;
 			let imgCenters = [];
-			for (let i = 0; i < document.images.length; i++) imgCenters.push(Math.abs(centerPos - imgCenter(document.images[i])));
+			for (let i = 0; i < images.length; i++) imgCenters.push(Math.abs(centerPos - imgCenter(images[i])));
+			activeImage = 0;
+			for (let i = 1; i < imgCenters.length; i++) if (imgCenters[i] < imgCenters[activeImage]) activeImage = i;
+		}
+		function scrollToActiveImage(){
+			let yPos = imgCenter(images[activeImage]) - window.innerHeight / 2;
+			document.documentElement.classList.remove('smooth-scroll');
+			window.scroll(0, yPos);
+			document.documentElement.classList.add('smooth-scroll');
+		}
+		function setMode1(){
+			radioMode1.checked = true;
+			itemMode1.classList.add('active');
+			itemMode2.classList.remove('active');
+			itemMode3.classList.remove('active');
+			tapeImages.classList.remove('ti-full-mode');
+			tapeImages.classList.remove('ti-thumb-mode');
+			scrollToActiveImage();
+		}
+		function setMode2(){
+			radioMode2.checked = true;
+			itemMode1.classList.remove('active');
+			itemMode2.classList.add('active');
+			itemMode3.classList.remove('active');
+			tapeImages.classList.remove('ti-thumb-mode');
+			tapeImages.classList.add('ti-full-mode');
+			scrollToActiveImage();
+		}
+		function setMode3(){
+			radioMode3.checked = true;
+			itemMode1.classList.remove('active');
+			itemMode2.classList.remove('active');
+			itemMode3.classList.add('active');
+			tapeImages.classList.remove('ti-full-mode');
+			tapeImages.classList.add('ti-thumb-mode');
+		}
+		function pressedNavKey(key){
+			if (images.length < 1) return;
+			if (tapeImages.classList.contains('ti-thumb-mode')) return;
+			let halfScreen = window.innerHeight / 2;
+			let centerPos = halfScreen + window.scrollY;
+			let imgCenters = [];
+			for (let i = 0; i < images.length; i++) imgCenters.push(Math.abs(centerPos - imgCenter(images[i])));
 			let min = 0;
 			for (let i = 1; i < imgCenters.length; i++) if (imgCenters[i] < imgCenters[min]) min = i;
 			let next = min + (key === 'q' ? -1 : 1);
-			if (next < 0) next = 0;
-			if (next > imgCenters.length - 1) next = imgCenters.length - 1;
-			let yPos = imgCenter(document.images[next]) - halfScreen;
+			next = Math.max(Math.min(next, imgCenters.length - 1), 0);
+			let yPos = imgCenter(images[next]) - halfScreen;
 			window.scroll(0, yPos);
 		}
 		function imgCenter(img){ return img.clientHeight / 2 + img.offsetTop; }
+		function imgClick(){
+			if (!tapeImages.classList.contains('ti-thumb-mode')) return;
+			this.classList.toggle('full-size');
+		}
+		function increaseThumb(){
+			if (!tapeImages.classList.contains('ti-thumb-mode')) return;
+			if (tapeImages.classList.contains('ti-thumb-gt3')) return;
+			let zoom = tapeImages.dataset.thumbsize || 0;
+			thumbSz.innerText = tapeImages.dataset.thumbsize = +zoom + 1;
+			changeThumbSizeClasses();
+		}
+		function decreaseThumb(){
+			if (!tapeImages.classList.contains('ti-thumb-mode')) return;
+			if (tapeImages.classList.contains('ti-thumb-lt3')) return;
+			let zoom = tapeImages.dataset.thumbsize || 0;
+			thumbSz.innerText = tapeImages.dataset.thumbsize = +zoom - 1;
+			changeThumbSizeClasses();
+		}
+		function changeThumbSizeClasses(){
+			tapeImages.classList.remove('ti-thumb-lt1');
+			tapeImages.classList.remove('ti-thumb-lt2');
+			tapeImages.classList.remove('ti-thumb-lt3');
+			tapeImages.classList.remove('ti-thumb-gt1');
+			tapeImages.classList.remove('ti-thumb-gt2');
+			tapeImages.classList.remove('ti-thumb-gt3');
+			let zoom = +tapeImages.dataset.thumbsize;
+			if (zoom > 0) tapeImages.classList.add('ti-thumb-gt' + zoom);
+			if (zoom < 0) tapeImages.classList.add('ti-thumb-lt' + -zoom);
+		}
+	}
+
+	function addTapeTools(){
+		let tapeTools = document.createElement('div');
+		tapeTools.setAttribute('id', 'tapeTools');
+		let html = '\n<span class="tt-handler"></span>\n';
+		html += '<span class="tt-mode active" id="ttm1">screen</span>\n';
+		html += '<span class="tt-mode" id="ttm2">full</span>\n';
+		html += '<span class="tt-mode" id="ttm3">thumb</span>\n';
+		html += '<span class="tt-thumb-size"><span id="thumbinc">+</span><span id="thumbdec">-</span>';
+		html += '<em id="thumbsz">0</em>\n';
+		tapeTools.innerHTML = html;
+		document.body.appendChild(tapeTools);
+		let x = localStorage.getItem('tapeToolsX');
+		let y = localStorage.getItem('tapeToolsY');
+		if (x && y) {
+			tapeTools.style.right = x;
+			tapeTools.style.top = y;
+		}
+		let ttHandler = tapeTools.querySelector('.tt-handler');
+		ttHandler.addEventListener('mousedown', ttHandlerMouseDown);
+		function ttHandlerMouseDown(e){
+			oX = e.layerX;
+			oY = e.layerY;
+			tapeTools.classList.add('active');
+			window.addEventListener('mousemove', ttHandlerMouseMove);
+			window.addEventListener('mouseup', ttHandlerMouseUp);
+		}
+		function ttHandlerMouseMove(e){
+			x = e.clientX;
+			y = e.clientY;
+			tapeTools.style.right = calcX();
+			tapeTools.style.top = y - oY + 'px';
+		}
+		function ttHandlerMouseUp(e){
+			window.removeEventListener('mousemove', ttHandlerMouseMove);
+			window.removeEventListener('mouseup', ttHandlerMouseUp);
+			tapeTools.classList.remove('active');
+			localStorage.setItem('tapeToolsX', calcX());
+			localStorage.setItem('tapeToolsY', y - oY + 'px');
+		}
+		function calcX(){ return document.body.clientWidth - x - ttHandler.clientWidth + oX + 'px'; }
 	}
 
 })();
